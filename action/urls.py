fd97b31d4a1507c3e6fae9edcd2c9c27d5638087
MatteoPossamai <mpossamaim@gmail.com>
2022-06-30 15:51:54 +0200

    Scratched first commands

--- action/urls.py
+++ action/urls.py
@@ -1,8 +1,27 @@
-from django.urls import include, path
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('slack/events', views.Events.as_view(), name='event_hook'),
+    path('top_zwappers/', views.TopZwappers.as_view(), name='top_zwappers'),
+    path('postmark_template_email/', views.PostmarkTemplateEmail.as_view(), name='postmark_template_email'),
+    path('postmark_plain_email/', views.PostmarkPlainTextEmail.as_view(), name='postmark_plain_email'),
+    path('query_users/', views.QueryUsers.as_view(), name='query_users'),
 ]
 
-#http://ee32-2001-b07-a5b-662a-d8f8-a8c7-bee7-2d21.ngrok.io/action/slack/events
+
+"""
+http://53a0-2001-b07-a5b-662a-a982-d45f-a305-1b48.ngrok.io/action/slack/events
+
+/top_zwappers
+http://53a0-2001-b07-a5b-662a-a982-d45f-a305-1b48.ngrok.io/action/top_zwappers/
+
+/postmark_template_email
+http://5982-2001-b07-a5b-662a-a982-d45f-a305-1b48.ngrok.io/action/postmark_template_email/
+
+/postmark_plain_email
+http://5982-2001-b07-a5b-662a-a982-d45f-a305-1b48.ngrok.io/action/postmark_plain_email/
+
+/query_users
+http://5982-2001-b07-a5b-662a-a982-d45f-a305-1b48.ngrok.io/action/query_users/
+"""
--- action/views.py
+++ action/views.py
@@ -1,24 +1,11 @@
-"""
-slack_event_adapter = SlackEventAdapter(settings.SIGNING_SECRET, "/slack/events")
-slack_client = slack.WebClient(token=settings.OAUTH_ACCESS_TOKEN)
-
-BOT_ID = slack_client.api_call("auth.test")["user_id"]
-
-@slack_event_adapter.on("message")
-def handle_message(payload):
-    event = payload.get("event", {})  # give info about what happened
-    channel = event.get("channel")
-    user = event.get("user")
-    text = event.get("text")
-    if user != BOT_ID:
-        slack_client.chat_postMessage(channel=channel, text=f"You said {text}")
-    return "Message received"
-"""
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from slack.errors import SlackApiError
+
 from zwapSlackProva import settings
 import slack
+import csv
 
 client = slack.WebClient(settings.OAUTH_ACCESS_TOKEN)
 SLACK_VERIFICATION_TOKEN = settings.SLACK_VERIFICATION_TOKEN
@@ -29,8 +16,6 @@ class Events(APIView):
     def post(self, request, *args, **kwargs):
 
         slack_message = request.data
-        print(slack_message.get('token'), SLACK_VERIFICATION_TOKEN)
-        print(slack_message)
 
         # if slack_message.get('token') != SLACK_VERIFICATION_TOKEN:
         # return Response(status=status.HTTP_403_FORBIDDEN)
@@ -41,10 +26,9 @@ class Events(APIView):
 
             # greet bot
         if 'event' in slack_message:
-            print(slack_message.get('event'))
             event_message = slack_message.get('event')
 
-            # ignore bot's own message
+            # ignore bots own message
             if event_message.get('subtype') == 'bot_message':
                 return Response(status=status.HTTP_200_OK)
 
@@ -58,3 +42,121 @@ class Events(APIView):
                 return Response(status=status.HTTP_200_OK)
 
         return Response(status=status.HTTP_200_OK)
+
+
+class TopZwappers(APIView):
+
+    def post(self, request, *args, **kwargs):
+        data = request.data
+        text = data.get('text')
+
+        try:
+            # Get the number of zwappers, according to the text or the default value
+            number = 10 if len(text) == 0 else int(text.split()[0])
+
+            # --------------------------------------------------
+            # Query on the db to retrieve the top zwappers
+            # ----------------------------------------------------
+
+            # Writing down on the csv file
+            with open('zwappers.csv', 'w', newline='') as csvfile:
+                writer = csv.writer(csvfile)
+                writer.writerow(['EMAIL', 'SUBSCRIPTION'])
+                writer.writerow([''])
+
+                for i in range(number):
+                    writer.writerow([f'Person {i}', i])
+
+                csvfile.close()
+
+            # Sending the csv file to the channel
+            result = client.files_upload(channels='#test', file='zwappers.csv')
+        except SlackApiError as e:
+            print(e.response)
+        except ValueError:
+            client.chat_postMessage(channel='#test', text='Argument must be an integer')
+
+        return Response(data='top_zwappers', status=status.HTTP_200_OK)
+
+
+class PostmarkTemplateEmail(APIView):
+
+    def post(self, request, *args, **kwargs):
+        data = request.data
+        text = data.get('text')
+
+        try:
+
+            postmark_template_id, csv_file_path = int(text.split()[0]), text.split()[1]
+            print(postmark_template_id, csv_file_path)
+
+            # Read the csv file
+            with open(csv_file_path, 'r') as csvfile:
+                reader = csv.reader(csvfile)
+                for row in reader:
+                    print(row)
+                    # --------------------------------------------------
+                    # Logic for sending the email
+                    # ----------------------------------------------------
+
+            # Confirmation of everithing went well
+            client.chat_postMessage(channel='#test', text="Emails have been sent correctly via PostMark")
+
+        except IndexError:
+            client.chat_postMessage(channel='#test', text="Please, provide both the postmark template id and the csv "
+                                                          "file path")
+        except FileNotFoundError:
+            client.chat_postMessage(channel='#test', text="The csv file doesn't exist")
+        except SlackApiError as e:
+            print(e.response)
+        except ValueError:
+            client.chat_postMessage(channel='#test', text="ID argument must be an integer")
+
+        return Response(data='postmark_template_email', status=status.HTTP_200_OK)
+
+
+class PostmarkPlainTextEmail(APIView):
+
+    def post(self, request, *args, **kwargs):
+        data = request.data
+        text = data.get('text')
+
+        try:
+
+            html_template_id, csv_file_path = int(text.split()[0]), text.split()[1]
+            print(html_template_id, csv_file_path)
+
+            # Read the csv file
+            with open(csv_file_path, 'r') as csvfile:
+                reader = csv.reader(csvfile)
+                for row in reader:
+                    print(row)
+                    # --------------------------------------------------
+                    # Logic for sending the email
+                    # ----------------------------------------------------
+
+            # Confirmation of everithing went well
+            client.chat_postMessage(channel='#test', text="Emails have been sent correctly via PostMark")
+
+        except IndexError:
+            client.chat_postMessage(channel='#test', text="Please, provide both the postmark template id and the csv "
+                                                          "file path")
+        except FileNotFoundError:
+            client.chat_postMessage(channel='#test', text="The csv file doesn't exist")
+        except SlackApiError as e:
+            print(e.response)
+        except ValueError:
+            client.chat_postMessage(channel='#test', text="ID argument must be an integer")
+
+        return Response(data='postmark_template_email', status=status.HTTP_200_OK)
+
+
+feature_types = ['open_to_tag', 'status_tag', 'n_meeting', 'n_matches', 'subscription_datetime', 'city', 'company',
+                 'role', 'gender', 'age']
+
+class QueryUsers(APIView):
+
+    def post(self, request, *args, **kwargs):
+        # Get the text from the request
+        data = request.data
+        text = data.get('text')
